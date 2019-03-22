40d3ec987d72942c4384638c43a925d598fb9e94
SouzaRDG <souzardg@yahoo.com>
2019-03-22 02:21:09 -0300

    removeadd

--- AutoGit.sh
+++ AutoGit.sh
@@ -2,9 +2,15 @@
 #
 # It will add the whole folder them make a commit and push
 # Just because im kinda lazy and forget to make a commit and push after add
-# 
+#
 git add *
 echo "Arquivos adicionados" 
+#
+# Be careful cause it will remove all ignored files 
+# (delete them permanently)
+git clean -f
+echo "Removidos arquivos excluídos"
+#
 #
 #    First variable is for the commit name
 git commit -m $1
---
+++ tessst.md
@@ -0,0 +1,2 @@
+fdsfdfssdfsdfsdsdsd
+
