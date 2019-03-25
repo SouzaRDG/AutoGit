b325b38343e73a1a77592b40aed63b65ebdec1c7
SouzaRDG <souzardg@yahoo.com>
2019-03-25 00:32:25 -0300

    gitinit

--- AutoGit.sh
+++ AutoGit.sh
@@ -2,20 +2,33 @@
 #
 # It will add the whole folder them make a commit and push
 # Just because im kinda lazy and forget to make a commit and push after add
-#
+
+
+# Checking if git is initialized, else it will initialize git
+
+if [! -d "/home/souzardg/Projetos/Shell/AutoGitPush/.git" && echo "Git not initialized, initializing Git"]
+then
+	git init
+else
+	echo "Git already initialized, adding archives"
+fi
+
+
 git add *
+
 echo "Arquivos adicionados" 
-#
+
 # Be careful cause it will remove all ignored files 
 # (delete them permanently)
+
 git add -u :/
 echo "Removidos arquivos excluídos"
-#
-#
+
+
 #    First variable is for the commit name
+
 git commit -m $1
+
 #
 #
-git push origin master
-#
-#
+git push origin master#
